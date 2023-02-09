7bf839b3c5b1b9e6a9090b9372de46445651e2d6
Carl Labuschagne <carl@carllabuschagne.com>
2023-02-09 09:44:56 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+console.log("app called");
+ 
+
+function Element(props) {
+  return (
+    <div className='wrapper'>
+      <h1>{props.symbol}</h1>
+      <h3>{props.no}</h3>
+      <h3>{props.name}</h3>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Element symbol="He" name="Helium" no="1"></Element>
+      <Element symbol="O" name="Oxygen" no="2"></Element>
+      <Element symbol="Br" name="Berilium" no="3"></Element>
+      <Element symbol="C" name="Carbon" no="4"></Element>
+      <Element symbol="He" name="Helium" no="5"></Element>
+      <Element symbol="He" name="Helium" no="6"></Element>
+      <Element symbol="He" name="Helium" no="7"></Element>
+      <Element symbol="He" name="Helium" no="8"></Element>
+      <Element symbol="He" name="Helium" no="9"></Element>
+      <Element symbol="O" name="Oxygen" no="10"></Element>
+      <Element symbol="Br" name="Berilium" no="11"></Element>
+      <Element symbol="C" name="Carbon" no="12"></Element>
+      <Element symbol="He" name="Helium" no="13"></Element>
+      <Element symbol="He" name="Helium" no="14"></Element>
+      <Element symbol="He" name="Helium" no="15"></Element>
+      <Element symbol="He" name="Helium" no="16"></Element>
+      <Element symbol="He" name="Helium" no="17"></Element>
+      <Element symbol="He" name="Helium" no="18"></Element>
+    
+<br />
+
+<Element symbol="He" name="Helium" no="1"></Element>
+      <Element symbol="O" name="Oxygen" no="2"></Element>
+      <Element symbol="Br" name="Berilium" no="3"></Element>
+      <Element symbol="C" name="Carbon" no="4"></Element>
+      <Element symbol="He" name="Helium" no="5"></Element>
+      <Element symbol="He" name="Helium" no="6"></Element>
+      <Element symbol="He" name="Helium" no="7"></Element>
+      <Element symbol="He" name="Helium" no="8"></Element>
+      <Element symbol="He" name="Helium" no="9"></Element>
+      <Element symbol="O" name="Oxygen" no="10"></Element>
+      <Element symbol="Br" name="Berilium" no="11"></Element>
+      <Element symbol="C" name="Carbon" no="12"></Element>
+      <Element symbol="He" name="Helium" no="13"></Element>
+      <Element symbol="He" name="Helium" no="14"></Element>
+      <Element symbol="He" name="Helium" no="15"></Element>
+      <Element symbol="He" name="Helium" no="16"></Element>
+      <Element symbol="He" name="Helium" no="17"></Element>
+      <Element symbol="He" name="Helium" no="18"></Element>
+    
     </div>
   );
 }
