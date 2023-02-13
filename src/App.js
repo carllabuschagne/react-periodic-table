2652f9c64b86025d04a141046a0f00f26e350371
Carl Labuschagne <carl@carllabuschagne.com>
2023-02-13 13:42:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data/elements.json';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data/elements.json";
 
 console.log("app called");
 
- 
-
+const Styles = {
+  item: {
+    margin: "0px",
+  },
+};
 
 function Element(props) {
- 
-  return (
-
-    data.map((data) => (
- 
-      <div className={"element element-"+data.number}>
-        <h3>{data.number}</h3>
-        <h1>{data.symbol}</h1>
-        <h3>{data.atomic_mass}</h3>
-      </div> 
-    ))
-);
+  return data.map((data) => (
+    <div className={"element element-" + data.number}>
+      <h2 style={Styles.item}>{data.number}</h2>
+      <h1 style={Styles.item}>{data.symbol}</h1>
+      <h3 style={Styles.item}>{data.name}</h3>
+      <h3 style={Styles.item}>{data.atomic_mass}</h3>
+    </div>
+  ));
 }
-
 
 function App() {
   return (
     <div className="App">
       <Element />
-      
-      
-    
     </div>
   );
 }
